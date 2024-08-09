d682cc0567904d8c5e0066f18eff9b65a000533a
AlexDelei <alexdelei125@gmail.com>
2024-08-09 11:45:57 +0300

    calling the class methods

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,11 +3,11 @@ const dbClient = require('../utils/db');
 
 class AppController {
   static getStatus(request, response) {
-    if (redisClient.isAlive() && dbClient.isAlive()) {
+    if (redisClient.isAlive && dbClient.isAlive) {
       return response.status(200).send({ redis: true, db: true });
     }
     return response.status(500).send(
-      { redis: redisClient.isAlive(), db: dbClient.isAlive() },
+      { redis: redisClient.isAlive, db: dbClient.isAlive },
     );
   }
 
