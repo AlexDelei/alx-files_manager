2711ba9437250b8dcd126bfc0b711a960d4044fe
AlexDelei <alexdelei125@gmail.com>
2024-08-09 09:58:43 +0300

    fixed some coding style issues

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,21 @@
-const redisClient = require('../utils/redis')
+const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class AppController {
   static getStatus(request, response) {
     if (redisClient.isAlive() && dbClient.isAlive()) {
-      return response.status(200).send({'redis': true, 'db': true});
+      return response.status(200).send({ redis: true, db: true });
     }
+    return response.status(500).send(
+      { redis: redisClient.isAlive(), db: dbClient.isAlive() },
+    );
   }
 
   static async getStats(request, response) {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
-    
-    return response.status(200).send({users, files});
+
+    return response.status(200).send({ users, files });
   }
 }
 
