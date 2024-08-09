ce1566ad1d190d0dca1a19b6f1ee0c0823a19965
AlexDelei <alexdelei125@gmail.com>
2024-08-09 09:50:21 +0300

    status of the database and statistics

---
+++ controllers/AppController.js
@@ -0,0 +1,19 @@
+const redisClient = require('../utils/redis')
+const dbClient = require('../utils/db');
+
+class AppController {
+  static getStatus(request, response) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      return response.status(200).send({'redis': true, 'db': true});
+    }
+  }
+
+  static async getStats(request, response) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    
+    return response.status(200).send({users, files});
+  }
+}
+
+module.exports = AppController;
