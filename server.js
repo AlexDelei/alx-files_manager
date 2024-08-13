0880062a76368f2d23b487e593d6a95745a16af2
AlexDelei <alexdelei125@gmail.com>
2024-08-13 19:50:50 +0300

    proper coding style

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import router from './routes/index';
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(router);
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
