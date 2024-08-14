679bdf9b7f2e733efab5c0e759bddbba4cd6f563
AlexDelei <alexdelei125@gmail.com>
2024-08-14 09:55:00 +0300

    validating the body sent in the request

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,28 +3,7 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body;
-
-    if (!email) {
-      return res.status(400).send({ error: 'Missing email' });
-    }
-    if (!password) {
-      return res.status(400).send({ error: 'Missing password' });
-    }
-    const alreadyExists = await dbClient.collection('users').findOne({ email });
-    if (alreadyExists) {
-      return res.status(400).send({ error: 'Already exist' });
-    }
-    const securePassword = sha1(password);
-    const newUser = { email, password: securePassword };
-    const saved = await dbClient.collection('users').insertOne(newUser);
-
-    return res.status(201).json(
-      {
-        id: saved.insertedId,
-        email: saved.email,
-      },
-    );
+    return res.status(200).send(req.body);
   }
 }
 
