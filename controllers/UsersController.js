ef43df4ca6fd956d9245547b8c3e7f427ccc28d0
AlexDelei <alexdelei125@gmail.com>
2024-08-14 10:31:11 +0300

    using the db client instance to make write operations to the database

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -11,13 +11,13 @@ class UsersController {
     if (!password) {
       return res.status(400).send({ error: 'Missing password' });
     }
-    const alreadyExists = await dbClient.collection('users').findOne({ email });
+    const alreadyExists = await dbClient.db.collection('users').findOne({ email });
     if (alreadyExists) {
       return res.status(400).send({ error: 'Already exist' });
     }
     const securePassword = sha1(password);
     const newUser = { email, password: securePassword };
-    const saved = await dbClient.collection('users').insertOne(newUser);
+    const saved = await dbClient.db.collection('users').insertOne(newUser);
 
     return res.status(201).json(
       {
