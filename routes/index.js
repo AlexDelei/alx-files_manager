45b8652b2c90cdcaf9f81737fcc2a7f79f3b475f
AlexDelei <alexdelei125@gmail.com>
2024-08-15 18:40:32 +0300

    endpoints configurations

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -10,7 +11,14 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
 
 router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/putUnpublish', FilesController.putUnpublish);
 
 export default router;
