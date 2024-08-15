baa30ac3d7744d3f19fc6f6a20f01f8a8a64a3db
AlexDelei <alexdelei125@gmail.com>
2024-08-15 18:46:07 +0300

    fixed coding style

--- utils/basic.js
+++ utils/basic.js
@@ -1,14 +1,14 @@
 import { ObjectId } from 'mongodb';
 
 const basicUtility = {
-    isValid(id) {
-        try {
-            ObjectId(id);
-        } catch (error) {
-            return false;
-        }
-        return true;
-    },
-}
+  isValid(id) {
+    try {
+      ObjectId(id);
+    } catch (error) {
+      return false;
+    }
+    return true;
+  },
+};
 
 export default basicUtility;
