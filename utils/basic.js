44be180cf75cbd812ce7f7a83a9ce3b4a8e105f8
AlexDelei <alexdelei125@gmail.com>
2024-08-15 18:42:21 +0300

    validates an id is of type ObjectId from mongodb

---
+++ utils/basic.js
@@ -0,0 +1,14 @@
+import { ObjectId } from 'mongodb';
+
+const basicUtility = {
+    isValid(id) {
+        try {
+            ObjectId(id);
+        } catch (error) {
+            return false;
+        }
+        return true;
+    },
+}
+
+export default basicUtility;
